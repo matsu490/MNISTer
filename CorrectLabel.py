25e055fe1f90eae0f9c7a43dae272f20eff37772
matsu490 <imototoriwatomodatijanai@gmail.com>
2017-01-18 15:52:38 +0900

    deleted CorrectLabel.py

--- CorrectLabel.py
+++
@@ -1,7 +0,0 @@
-from PyQt4 import QtGui, QtCore
-
-class CorrectLabel(QtGui.QComboBox):
-    def __init__(self, parent=None):
-        super(CorrectLabel, self).__init__()
-        for i in range(10):
-            self.addItem(str(i))
