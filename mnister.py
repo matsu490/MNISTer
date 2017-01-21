4537a5f2384c53f03a6590fd6378533d267ba416
matsu490 <imototoriwatomodatijanai@gmail.com>
2017-01-21 18:38:51 +0900

    updated MNISTer to be able to predict a digit by convolution net

--- mnister.py
+++ mnister.py
@@ -5,6 +5,7 @@ import PyQt4.uic
 import cv2
 import numpy as np
 import pickle
+from collections import OrderedDict
 
 uifile = './mainUI.ui'
 form, base = PyQt4.uic.loadUiType(uifile)
@@ -154,8 +155,9 @@ class MainUI(base, form):
         self.current_colour = Colour3(0,0,0)
         self.shape_num = 0
         self.current_width = 20
-        params = self.unpickle('params.pkl')
-        self.network = TwoLayerNet(params)
+        params = self.unpickle('params_trained_simpleconv.pkl')
+        # self.network = TwoLayerNet(params)
+        self.network = SimpleConvNet(params=params)
 
     def initClassLabels(self):
         for i in xrange(10):
@@ -228,6 +230,8 @@ class MainUI(base, form):
 
 
 def sigmoid(x):
+    # measure to deal with overflow
+    x = np.clip(x, -709, 100000)
     return 1. / (1. + np.exp(-x))
 
 
@@ -315,6 +319,304 @@ class TwoLayerNet(object):
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
         return grads
+
+
+class Relu(object):
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+class Affine(object):
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.original_x_xhape = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.original_x_xhape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+        out = np.dot(self.x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        dx = dx.reshape(*self.original_x_xhape)
+        return dx
+
+
+class SoftmaxWithLoss(object):
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size:
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+        return dx
+
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    N, C, H, W = input_data.shape
+    out_h = (H + 2 * pad - filter_h) // stride + 1
+    out_w = (W + 2 * pad - filter_w) // stride + 1
+
+    img = np.pad(input_data, [(0, 0), (0, 0), (pad, pad), (pad, pad)], 'constant')
+    col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in xrange(filter_h):
+        y_max = y + stride * out_h
+        for x in xrange(filter_w):
+            x_max = x + stride * out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N * out_h * out_w, -1)
+    return col
+
+
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    N, C, H, W = input_shape
+    out_h = (H + 2 * pad - filter_h) // stride + 1
+    out_w = (W + 2 * pad - filter_w) // stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2 * pad + stride - 1, W + 2 * pad + stride - 1))
+    for y in xrange(filter_h):
+        y_max = y + stride * out_h
+        for x in xrange(filter_w):
+            x_max = x + stride * out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
+
+
+class Pooling(object):
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,))
+
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+
+        return dx
+
+
+class Convolution(object):
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+        self.x = None
+        self.col = None
+        self.col_W = None
+        
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2 * self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2 * self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0, 2, 3, 1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+
+class SimpleConvNet(object):
+    def __init__(self, params, input_dim=(1, 28, 28),
+            conv_param={'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+            hidden_size=100,
+            output_size=10,
+            weight_init_std=0.01):
+
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2 * filter_pad) / filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size / 2) * (conv_output_size / 2))
+
+        self.params = {}
+        self.params['W1'] = params['W1']
+        self.params['W2'] = params['W2']
+        self.params['W3'] = params['W3']
+        self.params['b1'] = params['b1']
+        self.params['b2'] = params['b2']
+        self.params['b3'] = params['b3']
+
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(
+                self.params['W1'],
+                self.params['b1'],
+                conv_param['stride'],
+                conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+
+        self.last_layer = SoftmaxWithLoss()
+
+    def initParams(self):
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['W2'] = weight_init_std * \
+            np.random.randn(pool_output_size, hidden_size)
+        self.params['W3'] = weight_init_std * \
+            np.random.randn(hidden_size, output_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['b3'] = np.zeros(output_size)
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def judge(self, x):
+        return np.argmax(self.predict(x.reshape(1, 1, 28, 28)))
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
+
+    def accuracy(self, x, t, batch_size=100):
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+        acc = 0.0
+        for i in xrange(int(x.shape[0] / batch_size)):
+            tx = x[i * batch_size:(i + 1) * batch_size]
+            tt = t[i * batch_size:(i + 1) * batch_size]
+            y = self.predict(tx)
+            y = np.argmax(y, axis=1)
+            acc += np.sum(y == tt)
+        return acc / x.shape[0]
+
+    def numerical_gradient(self, x, t):
+        loss_w = lambda w: self.loss(x, t)
+        grads = {} 
+        for idx in xrange(1, 4):
+            grads['W{}'.format(idx)] = numerical_gradient(loss_w, self.params['W{}'.format(idx)])
+            grads['b{}'.format(idx)] = numerical_gradient(loss_w, self.params['b{}'.format(idx)])
+        return grads
+
+    def gradient(self, x, t):
+        self.loss(x, t)
+        dout = 1
+        dout = self.last_layer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        grads = {}
+        grads['W1'] = self.layers['Conv1'].dW
+        grads['W2'] = self.layers['Affine1'].dW
+        grads['W3'] = self.layers['Affine2'].dW
+        grads['b1'] = self.layers['Conv1'].db
+        grads['b2'] = self.layers['Affine1'].db
+        grads['b3'] = self.layers['Affine2'].db
+        return grads
+
+    def save_params(self, file_name='params.pkl'):
+        params = {}
+        for key, val in self.params.items():
+            params[key] = val
+        with open(file_name, 'wb') as f:
+            pickle.dump(params, f)
+
+    def load_params(self, file_name='params.pkl'):
+        with open(file_name, 'rb') as f:
+            params = pickle.load(f)
+        for key, val in params.items():
+            self.params[key] = val
+        for i, key in enumerate(['Conv1', 'Affine1', 'Affine2']):
+            self.layers[key].W = self.params['W{}'.format(i + 1)]
+            self.layers[key].b = self.params['b{}'.format(i + 1)]
 
 if __name__ == '__main__':
     app = PyQt4.QtGui.QApplication(sys.argv)
